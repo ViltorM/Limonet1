37ec1433d638317bfb33d338bd5d54a5b927555c
ViktorM <malimon.vv@gmail.com>
2025-07-17 07:26:05 +0300

    Update translations.js

--- translations.js
+++ translations.js
@@ -1,4 +1,4 @@
-translations = {
+ translations = {
   ua: {
     logoSubtitle: "Футляри та кейси для різних потреб",
     cart_title: "Кошик",
@@ -7,7 +7,13 @@ translations = {
     choose_payment: "Оберіть спосіб оплати",
     card_payment: "На картку",
     cod_payment: "Накладений платіж",
-    submit_order: "Надіслати замовлення"
+    submit_order: "Надіслати замовлення",
+    fio_placeholder: "ПІБ",
+    phone_placeholder: "Телефон",
+    city_placeholder: "Населений пункт",
+    post_placeholder: "Відділення пошти",
+    comment_placeholder: "Коментар...",
+    featured_products: "Рекомендуємо"
   },
   ru: {
     logoSubtitle: "Футляры и кейсы для разных нужд",
@@ -17,6 +23,12 @@ translations = {
     choose_payment: "Выберите способ оплаты",
     card_payment: "На карту",
     cod_payment: "Наложенный платеж",
-    submit_order: "Отправить заказ"
+    submit_order: "Отправить заказ",
+    fio_placeholder: "ФИО",
+    phone_placeholder: "Телефон",
+    city_placeholder: "Населенный пункт",
+    post_placeholder: "Отделение почты",
+    comment_placeholder: "Комментарий...",
+    featured_products: "Рекомендуем"
   }
 };
